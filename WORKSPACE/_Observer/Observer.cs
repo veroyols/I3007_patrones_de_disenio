b59ca7db916ff3f60f320c1deac54b5eb1264370
veroyols <vero.scholz@gmail.com>
2021-05-28 01:36:49 -0300

    observer

---
+++ WORKSPACE/_Observer/Observer.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: veros
+ * Fecha: 26/5/2021
+ * Hora: 19:58
+ * 
+ */
+using System;
+using System.Collections.Generic;
+
+namespace _Observer {
+	
+	public interface Observador { 
+		void actualizar(Observado observado, int monto); //ventas 
+	} //gerente
+
+	public interface Observado { 
+		void agregarObservador(Observador observador);
+		void notificar(int monto); //jornada de ventas
+		//list
+	} //vendedor	
+
+	public class Gerente : Observador  {
+		
+		public List<Observado> mejores = new List<Observado>();
+		
+		public void actualizar(Observado VendObservado, int monto){
+			this.venta(monto,((Vendedor)VendObservado)); 
+		}
+		
+		public void cerrar () { 
+			Console.WriteLine("MEJORES VENDEDORES");
+		}
+		
+		public void venta (int monto, Vendedor vendedor) {
+			if (monto > 5000) {
+				if ((mejores.Count == 0) || (!mejores.Contains(vendedor))) {
+					mejores.Add(vendedor);
+				}
+				vendedor.aumentaBonus();
+			}
+		}	
+	}
+
+	public class Vendedor : Observado { 
+		
+		private int sueldoBasico;
+		private double bonus;
+		//observer
+		public List<Observador> observadores = new List<Observador>();		
+		public void agregarObservador(Observador observador){
+			observadores.Add(observador);
+		}
+		public void notificar(int monto) {
+			foreach (Observador o in observadores) {
+				o.actualizar(this, monto);
+			} 
+		}		
+		public void venta (int monto) {
+			Console.WriteLine("Ha vendido ${0}",monto);
+			this.notificar(monto);
+		}
+		public void aumentaBonus () {
+			bonus+=0.1;	
+		}		
+		
+		public int getSueldoBasico {
+			get { return sueldoBasico;}
+			set { sueldoBasico = value;}
+		} 
+		public double getBonus {
+			get { return bonus;}
+			set { bonus = value;}
+		}		
+	}
+}
--- WORKSPACE/_Observer/Program.cs
+++ WORKSPACE/_Observer/Program.cs
@@ -13,32 +13,7 @@ namespace _Observer {
 	public class Program {
 				
 		public static void Main(string[] args)	{
-			
-			//Inicialización
-			Terminal t = new Terminal();
-			Cajero[] cs = new Cajero[5];
-			for(int c = 0; c < 5; c++){
-				cs[c] = new Cajero(c+1);
-				t.agregarObservador(cs[c]);
-			}
-			for(int c = 0; c < 5; c++){
-				for(int d = 0; d < 5; d++){
-					if(c != d)
-						cs[c].agregarObservador(cs[d]);
-				}
-			}
-			
-			//Simulación
-			Random r = new Random();
-			for(int c = 1; c <= 100; c++){
-				if(r.NextDouble() > 0.4)
-					t.solicitar();
-				else{
-					int cj = r.Next(5);
-					cs[cj].llamar();
-				}
-			}
-			
+						
 			Console.Write("Programa finalizado correctamente.");
 			Console.ReadKey(true);
 		}
