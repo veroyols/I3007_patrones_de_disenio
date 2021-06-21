7c7ab5870fd453cceeb9399b5f870d2a5012b628
veroyols <vero.scholz@gmail.com>
2021-06-21 01:36:30 -0300

    resumen

---
+++ WORKSPACE/Practica7/Singleton.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: veros
+ * Fecha: 21/6/2021
+ * Hora: 00:16
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Practica7
+{
+	/// <summary>
+	/// Description of Singleton.
+	/// </summary>
+	public sealed class Singleton
+	{
+		private static Singleton instance = new Singleton();
+		
+		public static Singleton Instance {
+			get {
+				return instance;
+			}
+		}
+		
+		private Singleton()
+		{
+		}
+	}
+}
